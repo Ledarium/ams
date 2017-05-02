5b09806edb15bae3cabb341bb311301aefe68b06
Ledarium <nameleeees@gmail.com>
2017-05-03 01:30:56 +0300

    added categories

--- backup.py
+++ backup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from subprocess import call, Popen, PIPE
-from shlex import split
+import shlex
 from pathlib import Path
 from datetime import datetime
 import config
@@ -16,36 +16,29 @@ def backup():
         backup_path.mkdir()
     backup_location = backup_path / date
     backup_location.mkdir()
-    rsync_args = config.rsync_args;
-    if not rsync_args:
-        rsync_args == "--progress"
     if config.verbose_mode:
         print("Current backup will be saved in", backup_location)
+    if config.rsync_args:
+        global_args = config.rsync_args
+    else:
+        global_args == "--progress"
 
-    print("Backing up user configs")
-    for path in config.user_configs:
-        path = Path.home() / path
-        args = rsync_args.split()
-        call(['rsync',
-              *args,
-              str(path),
-              str(backup_location)])
+    def backup_category(category):
+        print("Backing up", category['name'])
+        location = backup_location / category['folder']
+        location.mkdir(exist_ok=True)
+        location = str(location)
+        for path in category['files']:
+            call(' '.join([
+                 category.get('prefix', ''), # prefix rsync command (for example with sudo)
+                 'rsync',
+                 category.get('args', global_args), # args passed to rsync
+                 path,
+                 location]), shell = True)
 
-    print("Backing up system configs")
-    for path in config.system_configs:
-        args = rsync_args.split()
-        call(['sudo', 'rsync',
-              *args,
-              str(path),
-              str(backup_location)])
+    for category in config.categories:
+        backup_category(category)
 
-    print("Backing up other paths")
-    for path in config.system_configs:
-        args = rsync_args.split()
-        call(['rsync',
-              *args,
-              str(path),
-              str(backup_location)])
 
 if __name__ == "__main__":
     backup()
--- example_config.py
+++ example_config.py
@@ -11,14 +11,18 @@ Enabled options for rsync are:
     -hhh: outputs numbers in human-readable format, in units of 1024 (K, M, G, T).
     --progress: show progress
 '''
-user_configs = [".vimrc",
-                ".zshrc",
-                ".zprofile",
-                ".gitconfig",
-               ]
-system_configs = ["/etc/pulse/default.pa",
-                  "/etc/makepkg.conf",
-                  "/etc/modprobe.d/alsa-base.conf",
-                 ]
-other_paths = [
-              ]
+user_configs = {'name': "user configs",
+                'folder': "user",
+                'files':   ["$HOME/.vimrc",
+                            "$HOME/.zshrc",
+                            "$HOME/.zprofile"]
+                }
+system_configs = {'name': "system configs",
+                  'prefix': "sudo",
+                  'folder': "system",
+                  'files': ["/etc/pulse/default.pa",
+                            "/etc/makepkg.conf",
+                            "/etc/modprobe.d/alsa-base.conf",
+                            "/etc/systemd/system/suspend@.service"]
+                 }
+categories = [user_configs, system_configs]

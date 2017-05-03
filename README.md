9057c348f252d10f6194c9fb869208d2a45e4083
Ledarium <nameleeees@gmail.com>
2017-05-03 14:19:41 +0300

    More docs!

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # ams
-Arch Maintain System
+Arch Maintain System - one solution to rule them all.
+
+### Planned feature list
+* Highly configurable backup system using rsync
+* Updating and maintaining packages
+* ... and other cool stuff.
+
+Currently only backup feature is implemented. Maybe you want to help?
 
 ## Installation
 Clone this repo and you already rock.
 
 ## Using
 Make your own `config.py` using `example_config.py` as an example.
-Run `run.py -h` for available options.
+    Run `run.py -h` for available options.
--- example_config.py
+++ example_config.py
@@ -10,21 +10,21 @@ Enabled options for rsync are:
     -hhh: outputs numbers in human-readable format, in units of 1024 (K, M, G, T).
     --progress: show progress
 '''
+system_configs = {'name': "system configs", # specifies name displayed in log
+                  'prefix': "sudo", # specifies how to prefix rsync command
+                  'folder': "system", # subfolder for this category inside your backup_path
+                  'args': "-v", # specifies additional rsync flag to this category
+                  'files': ["/etc/pulse/default.pa", # shell variables and specials are supported
+                            "/etc/makepkg.conf",
+                            "/etc/modprobe.d/alsa-base.conf",
+                            "/etc/systemd/system/suspend@.service"]
+                 }
 user_configs = {'name': "user configs",
                 'folder': "user",
                 'files':   ["$HOME/.vimrc",
                             "$HOME/.zshrc",
                             "$HOME/.zprofile"]
                 }
-system_configs = {'name': "system configs", # specifies name displayed in log
-                  'prefix': "sudo", # specifies how to prefix rsync command
-                  'folder': "system", # subfolder for this category inside your backup_path
-                  'args': "-v", # specifies additional flag to this category
-                  'files': ["/etc/pulse/default.pa", # shell variables and specials are supported
-                            "/etc/makepkg.conf",
-                            "/etc/modprobe.d/alsa-base.conf",
-                            "/etc/systemd/system/suspend@.service"]
-                 }
 backup = [user_configs, system_configs]
 
 ''' not implemented

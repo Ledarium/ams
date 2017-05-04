2ffd373f9cfd66f64b4b548a1de7d7b65bab31ad
Ledarium <nameleeees@gmail.com>
2017-05-04 23:47:39 +0300

    updated example config

--- example_config.py
+++ example_config.py
@@ -11,6 +11,15 @@ Enabled options for rsync are:
     -hhh: outputs numbers in human-readable format, in units of 1024 (K, M, G, T).
     --progress: show progress
 '''
+tar_args = "-cJvf"
+'''
+Enabled options for tar are:
+    -c: create archive
+    -J: compress with xz
+    -v: show progress
+    -f: specifies output filename (one will not need to remove it, seriously)
+'''
+
 system_configs = {
     'name': "system configs",  # specifies name displayed in log
     'prefix': "sudo",  # specifies how to prefix rsync command
@@ -20,15 +29,23 @@ system_configs = {
               "/etc/makepkg.conf",
               "/etc/modprobe.d/alsa-base.conf",
               "/etc/systemd/system/suspend@.service"]
-                 }
+}
 user_configs = {
     'name': "user configs",
     'folder': "user",
-    'relative_to': "$HOME",  # shell variables and specials are supported
+    'relative_to': "$HOME",
     'files': [".vimrc",
               ".zshrc",
               ".zprofile"]
-                }
+}
+music = {
+    'name': "music",
+    'folder': "music",
+    'relative_to': "$HOME/Music",
+    'files': ["*"]
+}
+
+
 backup = [user_configs, system_configs]
 
 remove_orphans = {'flags': "-Qdt"}
